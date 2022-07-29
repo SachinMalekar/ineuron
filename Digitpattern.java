f6ee2e0d00ab93a7641beab8915a24a2551b58da
SachinMalekar <55599572+SachinMalekar@users.noreply.github.com>
2022-07-29 18:15:28 +0530

    Add files via upload

---
+++ Digitpattern.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Digitpattern {
+    public static void main(String args[]){
+        int i=1,j=1;
+        for(i=1;i<=4;i++){
+            for(j=1;j<=4;j++){
+                System.out.print(" "+i);
+            }
+           System.out.println(" ");
+        }
+
+    }
+}
